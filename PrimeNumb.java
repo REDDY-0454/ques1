0d535c84c12eb9b36cd85bac5973e70e2b0985ae
REDDY0454 <22h51a0523@cmrcet.ac.in>
2024-12-12 11:58:20 +0530

    main

---
+++ PrimeNumb.java
@@ -0,0 +1,24 @@
+public class PrimeNumb 
+{
+    public static void main(String[] args)
+     {
+        int limit = 100;
+        for (int num = 2; num <= limit; num++)
+	 {
+            if (isPrime(num)) 
+            {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int n)
+     {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+         {
+            if (n % i == 0) return false;
+         }
+        return true;
+    }
+}
